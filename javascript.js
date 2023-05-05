147eccaf07b5ae70df2bac669c3d1f20ebd2cbab
davidbujok <davebujok@gmail.com>
2023-05-05 18:11:50 +0100

    Add rgb, black colouring options
    Rgb and black buttons to switch between
    colouring options. Clear button to reset
    the grid. Text box displays exact amount
    of squares in the grid.

--- javascript.js
+++ javascript.js
@@ -4,16 +4,36 @@ const navTitle = document.querySelector('.navTitle');
 const navbar = document.querySelector('.navbar');
 const slider = document.getElementById("sketchRange");
 const gridSizeOutput = document.querySelector('.gridSize');
+const rgb = document.querySelector('.rgb');
+const black = document.querySelector('.black');
+const text = document.querySelector('.text');
+const clear = document.querySelector('.clear');
 
-gridSizeOutput.innerHTML = slider.value;
-slider.oninput = (event) =>  gridSizeOutput.innerHTML = event.target.value;
+
+
+gridSizeOutput.innerHTML = "2601";
+slider.oninput = function() {
+    let howManySquares = slider.value * slider.value;
+    gridSizeOutput.innerHTML = howManySquares;
+}
+
+text.textContent = `That's how many "pixels" you have on the canvas!`
 
 
 // window.getComputedStyle(document.documentElement).getPropertyValue('--square-size');
 
-function drawOnClick() {
-        square.setAttribute('style', 'background-color: black;');
-        };
+function rgbPencil() {
+        const rgbRange = [];
+        const rgbColour = [];
+        for (i = 0; i <= 255; i++) { // Creating an array with numbers from 0 to 255
+            rgbRange.push(i);
+        }
+        for (i = 0; i < 3; i++) {
+            let rgbValue = Math.floor(Math.random() * rgbRange.length); // getting Random value from an array rgbRange
+            rgbColour.push(rgbValue)
+        }
+        return rgbColour;
+        }; // Creating an array with three random RGB values range 0-255
 
 
 function gridDraw(numberOfSquares) {
@@ -21,33 +41,50 @@ function gridDraw(numberOfSquares) {
     document.documentElement.style.setProperty('--square-size', squareRoot );
     let isMouseoverEnabled = false;
     let isSquareClicked = true;
+    let isRGBClicked = false; 
+    let isStyleRemoved = false;
+    rgb.addEventListener('click', function() {
+        isRGBClicked = true;
+        isSquareClicked = true;
+        isMouseoverEnabled = false;
+    })
+    black.addEventListener('click', function() {
+        isRGBClicked = false;
+        isSquareClicked = false;
+        isMouseoverEnabled = true;
+
+    })
+    clear.addEventListener('click', function() {
+        location.reload();
+    })
         for (i = 0; i<numberOfSquares; i++) {
             const square = document.createElement('div');
             square.classList.add('squareOutline');
             grid.appendChild(square);
             square.addEventListener('click', function() {
-                isSquareClicked = !isSquareClicked;
-                if (isSquareClicked) {
-                    isMouseoverEnabled = false;
-                    // square.setAttribute('style', 'background-color: red;');
-                } else {
-                    isMouseoverEnabled = true;
+                isStyleRemoved = !isStyleRemoved; 
+                console.log(isStyleRemoved);
+                if (!isStyleRemoved) {
                     square.removeAttribute('style');
                 }
-                console.log(isMouseoverEnabled);
-                console.log(isSquareClicked);
             });
     
             square.addEventListener('mouseover', function() {
-                if (isMouseoverEnabled && !isSquareClicked) {
+                if (isMouseoverEnabled && !isSquareClicked && !isRGBClicked && isStyleRemoved) {
                     square.setAttribute('style', 'background-color: black;');
+                } else if (isSquareClicked && isRGBClicked && !isMouseoverEnabled && isStyleRemoved) {
+                    let rgbColour = rgbPencil();
+                        let r = rgbColour[0];
+                        let g = rgbColour[1];
+                        let b = rgbColour[2];
+                        let rgb = `rgb(${r},${g},${b})`;
+                    square.style.setProperty('--square-color', rgb);
                 }
             });
+
         }
     }
             
-        
-
 
 slider.addEventListener('input', function(e) {
     if (slider.value) {
@@ -56,6 +93,18 @@ slider.addEventListener('input', function(e) {
         gridDraw(b);   
     }
 })
+
+// rgb.addEventListener('click', function() {
+//     let rgbColour = rgbPencil();
+//     let r = rgbColour[0];
+//     let g = rgbColour[1];
+//     let b = rgbColour[2];
+//     let rgb = `rgb(${r},${g},${b})`;
+//     console.log(r,g,b);
+//     square.addEventListener('mouseover', function() {
+//         square.style.setProperty('--square-color', rgb);
+//     })
+// })
 
 
 gridDraw(2601);
